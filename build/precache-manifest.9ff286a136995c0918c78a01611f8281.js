8ed9268bb42773914ee241d46f81b07b544134bd
Tobias Andre <tobiasae@gmail.com>
2019-04-04 15:18:34 -0300

    initial commit

---
+++ build/precache-manifest.9ff286a136995c0918c78a01611f8281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "333d2ab9dbe921fbd6f7",
+    "url": "/static/js/main.0f4eb64d.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "333d2ab9dbe921fbd6f7",
+    "url": "/static/css/main.3f396c37.chunk.css"
+  },
+  {
+    "revision": "911c265a014828ef92ba282c0a6688cc",
+    "url": "/index.html"
+  }
+];
